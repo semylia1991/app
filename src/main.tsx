1b8a27a0e6a9bff5981c16d9d5d3264d55c57a59
semylia1991 <yupar0501@gmail.com>
2026-03-23 10:17:44 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,7 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import posthog from 'posthog-js';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
- 
-posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-  api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://eu.i.posthog.com',
-  person_profiles: 'identified_only', // создаём профиль только для авторизованных
-});
  
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
